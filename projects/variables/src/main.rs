64abd7127d6cd8f9d481831ce8d0bc5b5c8ab271
Wes White <wtealwhite@gmail.com>
2019-02-05 20:29:57 -0600

    functions

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -3,4 +3,17 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    //const naming convension
+    const MAX_POINTS: u32 = 100_000;
+
+
+    //variable SHADOWING
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
 }
